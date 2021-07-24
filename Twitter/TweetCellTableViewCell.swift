1f58641d8cd51649214b9c71c66116450fe28bfe
sowmya reddy <atlasowmya14@gmail.com>
2021-07-24 00:39:59 -0500

    change in tweet cell

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class TweetCellTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
